6d741cd1df731e1577652d11f0f0a78f7b5a192a
Sergey Zorych <sergey.zorych@gmail.com>
2021-10-04 00:02:49 +0300

    refactor(films): change function name

--- app/src/main/java/com/sergey_zorych/tmdb/Application.kt
+++ app/src/main/java/com/sergey_zorych/tmdb/Application.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.sergey_zorych.tmdb.di.mergedModulesList
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 import timber.log.Timber
 
@@ -17,19 +18,19 @@ class Application : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        startKoin()
-        startTimber()
+        initKoin()
+        initTimber()
     }
 
-    private fun startKoin() {
-        org.koin.core.context.startKoin {
+    private fun initKoin() {
+        startKoin {
             androidLogger(Level.ERROR)
             androidContext(this@Application)
             modules(mergedModulesList)
         }
     }
 
-    private fun startTimber() {
+    private fun initTimber() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
