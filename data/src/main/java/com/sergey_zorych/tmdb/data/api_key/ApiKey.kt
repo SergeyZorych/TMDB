2073dcd350f94d278999adb1d135af82c30ace11
Sergey Zorych <sergey.zorych@gmail.com>
2021-10-07 22:27:02 +0300

    docs(data): update

--- data/src/main/java/com/sergey_zorych/tmdb/data/api_key/ApiKey.kt
+++ data/src/main/java/com/sergey_zorych/tmdb/data/api_key/ApiKey.kt
@@ -3,6 +3,8 @@ package com.sergey_zorych.tmdb.data.api_key
 /**
  * Created by Android Studio on 10/7/21 5:33 PM
  *
+ * Class that contains value of api key
+ *
  * @author Sergey Zorych
  */
 data class ApiKey(
