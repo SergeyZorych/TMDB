92671c80fc1b120270794d3b3c6cddcec916293c
Sergey Zorych <sergey.zorych@gmail.com>
2021-10-07 22:14:32 +0300

    docs(navigation): update

--- libraries/navigation/src/main/java/com/sergey_zorych/tmdb/navigation/Directions.kt
+++ libraries/navigation/src/main/java/com/sergey_zorych/tmdb/navigation/Directions.kt
@@ -5,6 +5,10 @@ import com.sergey_zorych.tmdb.navigation.base.BaseDirection
 /**
  * Created by Android Studio on 10/5/21 3:07 PM
  *
+ * Represents all possible destinations in application. Direction must implementation [BaseDirection]
+ * interface for define correct deeplink uri. Every direction may contains object that define
+ * possible "in" and "out" bundle keys.
+ *
  * @author Sergey Zorych
  */
 object Directions {
