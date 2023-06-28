9e5902e34019eb001cbc5fd093a3a21080abfee8
Sergey Zorych <sergey.zorych@gmail.com>
2023-06-28 17:38:12 +0300

    BUILD: Add build convention plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,22 @@
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+    includeBuild("build-logic")
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        jcenter() // Warning: this repository is going to shut down soon
     }
 }
+
 rootProject.name = "TMDB"
 include(":app")
 include(":domain")
